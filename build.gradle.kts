2dd2e23a86f10924304db1ee40bac59b0889214e
su080915 <tbr335@naver.com>
2025-09-20 11:00:33 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.moimmee"
+version = "0.0.1-SNAPSHOT"
+description = "api-gateway"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.0.0"
+
+dependencies {
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // health check
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // reactive redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+
+    // security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
+
+    // data 통신
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    // spring cloud
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webmvc")
+
+    // monitoring
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
